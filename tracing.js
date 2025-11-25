9997d38aa0fc0940c94a940d805241ba832c3429
kbpop <kp.petzold.bradley@gmail.com>
2025-11-24 21:04:59 -0500

    Assignment

---
+++ tracing.js
@@ -0,0 +1,47 @@
+const { resourceFromAttributes } = require("@opentelemetry/resources");
+const { ATTR_SERVICE_NAME } = require("@opentelemetry/semantic-conventions");
+const { NodeSDK } = require("@opentelemetry/sdk-node");
+
+const {
+  getNodeAutoInstrumentations,
+} = require("@opentelemetry/auto-instrumentations-node");
+
+const {
+  OTLPTraceExporter,
+} = require("@opentelemetry/exporter-trace-otlp-http");
+const {
+  OTLPMetricExporter,
+} = require("@opentelemetry/exporter-metrics-otlp-http");
+
+const { PeriodicExportingMetricReader } = require("@opentelemetry/sdk-metrics");
+
+const {
+  ExpressInstrumentation,
+} = require("opentelemetry-instrumentation-express");
+const {
+  MongoDBInstrumentation,
+} = require("@opentelemetry/instrumentation-mongodb");
+const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
+
+const traceExporter = new OTLPTraceExporter({});
+
+const metricExporter = new OTLPMetricExporter({});
+
+const sdk = new NodeSDK({
+  resource: resourceFromAttributes({
+    [ATTR_SERVICE_NAME]: "todo-service",
+  }),
+  traceExporter,
+  metricReader: new PeriodicExportingMetricReader({
+    exporter: metricExporter,
+  }),
+  instrumentations: [
+    getNodeAutoInstrumentations(),
+    new ExpressInstrumentation(),
+    new MongoDBInstrumentation(),
+    new HttpInstrumentation(),
+  ],
+});
+
+// Start the SDK
+sdk.start();
